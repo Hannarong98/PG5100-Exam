c825925272d812a50528f7ba516d748a737b63a1
Hannarong Klinjan <hannarong98@hotmail.com>
2020-05-07 02:48:53 +0200

    Add default datas

--- backend/src/main/java/no/kristiania/exam/pg5100/backend/service/DefaultDataInitializerService.java
+++ backend/src/main/java/no/kristiania/exam/pg5100/backend/service/DefaultDataInitializerService.java
@@ -29,9 +29,36 @@ public class DefaultDataInitializerService {
     @PostConstruct
     public void initialize() {
 
+        //Default user
         attempt(() -> userService.createUser("foo@chocolatebar.baz", "123", "foo", "chocobar"));
-        Long id = attempt(()-> itemService.createItem("Some", Rarity.RARE, "some rare item", 1000));
-        attempt(()-> inventoryService.addItemToCollectionList("foo@chocolatebar.baz", id));
+
+        //Legendary cards
+        attempt(()-> itemService.createItem("Evocation", Rarity.LEGENDARY, "Fill your hand with random mage spells.", 1000));
+        attempt(()-> itemService.createItem("Reliquary of Souls", Rarity.LEGENDARY, "some rare item", 1000));
+        attempt(()-> itemService.createItem("Untapped Potential", Rarity.LEGENDARY, "some rare item", 1000));
+        attempt(()-> itemService.createItem("Astromancer Solarian", Rarity.LEGENDARY, "some rare item", 1000));
+        attempt(()-> itemService.createItem("Bloodmage Thalnos", Rarity.LEGENDARY, "some rare item", 1000));
+
+        //Epic cards
+        attempt(()-> itemService.createItem("Ancestor Call", Rarity.EPIC, "Put a random minion from each player's hand into the battlefield.", 750));
+        attempt(()-> itemService.createItem("Academic Espionage", Rarity.EPIC, "Shuffle 10 cards from your opponent's clsas into your deck.", 750));
+        attempt(()-> itemService.createItem("Anubisath Defender", Rarity.EPIC, "Taunt that cost 0. Costs if you've cast a spell that or more this turn.", 750));
+        attempt(()-> itemService.createItem("A New Challenger", Rarity.EPIC, "Discover: a 6-Cost minion. Summon it with Taunt and Divine Shield", 750));
+        attempt(()-> itemService.createItem("Ancient of War", Rarity.EPIC, "Choose One: +5 Attack or +5 Health and Taunt", 750));
+
+        //Rare cards
+        attempt(()-> itemService.createItem("Air Raid", Rarity.RARE, "Twinspell: summon two 1/1 Silver Hand Recruits with Taunt", 500));
+        attempt(()-> itemService.createItem("Aldo Peacekeeper", Rarity.RARE, "Battlecry: change an enemy minion's attack to 1", 500));
+        attempt(()-> itemService.createItem("Alexstrasza's Champion", Rarity.RARE, "If you're holding a Dragon, gain 01 Attack and Charge", 500));
+        attempt(()-> itemService.createItem("Ambush", Rarity.RARE, "Secret: After your opponent plays a minion, summon a 2/3 Ambusher with Poisonous", 500));
+        attempt(()-> itemService.createItem("Angry Chicken", Rarity.RARE, "Enrage: +5 Attack.", 500));
+
+        //Common cards
+        attempt(()-> itemService.createItem("A Ligth in the Darkness", Rarity.COMMON, "Discover: a minion and give it +1/+1.", 250));
+        attempt(()-> itemService.createItem("Acolyte of Pain", Rarity.COMMON, "Whenever this minion takes damage, draw a card", 250));
+        attempt(()-> itemService.createItem("Adaptation", Rarity.COMMON, "Adapt a friendly minion", 250));
+        attempt(()-> itemService.createItem("Aeon Reaver", Rarity.COMMON, "Battlecry: Deal damage to a minion equal to its Attack", 250));
+        attempt(()-> itemService.createItem("Amber Watcher", Rarity.COMMON, "Battlecry: Restore 8 Health", 250));
 
     }
 
--- backend/src/main/java/no/kristiania/exam/pg5100/backend/service/ItemService.java
+++ backend/src/main/java/no/kristiania/exam/pg5100/backend/service/ItemService.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import java.util.Collections;
+import javax.persistence.TypedQuery;
+import java.util.*;
 
 @Service
 @Transactional
@@ -32,5 +33,45 @@ public class ItemService {
 
     public Item getItem(long itemId){
         return em.find(Item.class, itemId);
+    }
+
+    public List<Item> getAllItems(){
+        TypedQuery<Item> query = em.createQuery("select i from Item i", Item.class);
+
+        return query.getResultList();
+    }
+
+    public List<Item> getRandomItems(int n, long categoryId){
+
+        TypedQuery<Long> sizeQuery= em.createQuery("select count(i) from Item i where i.id=?1", Long.class);
+        sizeQuery.setParameter(1, categoryId);
+        long size = sizeQuery.getSingleResult();
+
+        if(n > size){
+            throw new IllegalArgumentException("Cannot choose " + n + " unique items out of the " + size + " existing");
+        }
+
+        Random random = new Random();
+
+        List<Item> items = new ArrayList<>();
+        Set<Integer> chosen = new HashSet<>();
+
+        while(chosen.size() < n) {
+
+            int k = random.nextInt((int)size);
+            if(chosen.contains(k)){
+                continue;
+            }
+            chosen.add(k);
+
+            TypedQuery<Item> query = em.createQuery(
+                    "select i from Item i where i.id=?1", Item.class);
+            query.setParameter(1, categoryId);
+            query.setMaxResults(1);
+            query.setFirstResult(k);
+
+            items.add(query.getSingleResult());
+        }
+        return  items;
     }
 }
